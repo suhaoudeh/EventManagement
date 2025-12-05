0dc2180656f56c3190de67864d242cc5bbb3ea8f
suhaoudeh <soudeh@my.centennialcollege.ca>
2025-12-04 19:20:26 -0500

    update the api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',  // Set base URL from environment variable or default to localhost
+ baseURL: '/api',  // Set base URL from environment variable or default to localhost
   headers: {
     'Content-Type': 'application/json',
   },
